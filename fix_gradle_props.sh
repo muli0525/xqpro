3e0f3ffd86545107e20591e4e2672eb1e9280b8c
ChessPro <chesspro@example.com>
2026-02-11 19:40:05 +0800

    Add launcher icons - ic_launcher vector drawable

---
+++ fix_gradle_props.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+cd /f/test/ChineseChessPro
+git add gradle.properties
+git commit -m "Add gradle.properties - enable AndroidX"
+git push
---
+++ fix_icons.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+cd /f/test/ChineseChessPro
+git add -A
+git commit -m "Add launcher icons - ic_launcher vector drawable"
+git push
